24e1d05f1f89234aa395ba9cadf25e67ce76fb0c
Sudhanshu Singh <111054333+sudhanshusinghss@users.noreply.github.com>
2026-08-15 15:47:15 +0530

    Solve: 54. Spiral Matrix

---
+++ solutions/0054-spiral-matrix.py
@@ -0,0 +1,11 @@
+while(top<=bottom && left<=right){
+        List ans=new ArrayList<>();
+
+        int left = 0;
+        int right = m-1;
+        int bottom=n-1;
+        int top=0;
+        int m=matrix[0].length;
+        int n=matrix.length;
+    public List<Integer> spiralOrder(int[][] matrix) {
+class Solution {
